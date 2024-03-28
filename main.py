de981f4d27a834e3c72969cf1ef9df128dc34e37
Mustenaka <mumten120@outlook.com>
2024-03-28 17:08:58 +0800

    use

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-import unrar
-import unzip
 import os
-import sys
+os.environ.setdefault(
+        "UNRAR_LIB_PATH", "/rarlib/libunrar.so")   # TEMP: use .sh or .bat to export it
 
-def main():
-    print("Start Project")
+from src.dic_unpack.dic_unpack import DicUnpack
+
+
+def main(file_name):
+    # open file
+    if not os.path.isfile(file_name):
+        print("file does not exist.")
+
+    # use Dictionary Unpack
+    dic_unpack = DicUnpack("password_list")
+    if file_name.endswith('.rar'):
+        dic_unpack.rar_attack(file_name)
+    elif file_name.endswith('.zip'):
+        dic_unpack.zip_attack(file_name)
+
 
 if __name__ == "__main__":
-    main()
+
+    # file_name = sys.argv[1]
+    # main(file_name=file_name)
+    main(file_name="origin_file/test.rar")
