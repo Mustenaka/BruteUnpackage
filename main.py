5516a9d0e2d1dd67f32c80542d4ae20669fa57bd
Mustenaka <mumten120@outlook.com>
2024-04-11 15:48:19 +0800

    add main argparse

--- main.py
+++ main.py
@@ -1,26 +1,71 @@
 import argparse
 import os
 os.environ.setdefault(
-        "UNRAR_LIB_PATH", "/Users/andrew/Projects/PythonProjects/Tools/brute_force_unpackage/rarlib/libunrar.so")
-
+    "UNRAR_LIB_PATH", "/Users/andrew/Projects/PythonProjects/Tools/brute_force_unpackage/rarlib/libunrar.so")
 from src.dic_unpack.dic_unpack import DicUnpack
 
+def arg_resolving():
+    # create ArgumentParser object
+    parser = argparse.ArgumentParser(
+        description="Brute force cracking the compressed package")
 
-def main(file_name):
+    # add must being param
+    parser.add_argument('-f', '--file', help="unpackage file")
+
+    # add optional param
+    parser.add_argument(
+        '-d', '--dic',
+        help="use password dictionary, false is not use, true is use dictionary",
+        action='store_true')
+    parser.add_argument(
+        '-p', '--path',
+        help="dictionary path, default is inner dictionary, path should be a folder",
+        action='store_true')
+
+    parser.add_argument(
+        '-b', '--brust',
+        help="Use generate numeric sequential traversal, false is not use, true is use",
+        action='store_true')
+    parser.add_argument(
+        '-u', '--uppercase',
+        help="generated using uppercase letters,", action='store_true')
+    parser.add_argument(
+        '-l', '--lowercase',
+        help="generated using lowercase letters", action='store_true')
+    parser.add_argument(
+        '-n', '--number',
+        help="generated using numbers", action='store_true')
+    parser.add_argument(
+        '-s', '--simbol',
+        help="generated using special simbol", action='store_true')
+
+    parser.add_argument(
+        '-t', '--thread',
+        help="thread usage, default is 1", action='store_true')
+
+    # 解析命令行参数
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    # 解析命令行参数
+    file_name = args.file
+    print("unpackage file path" + file_name)
+
     # open file
     if not os.path.isfile(file_name):
-        print("file does not exist.")
+        print("package file does not exist.")
         return
 
     # use Dictionary Unpack
-    dic_unpack = DicUnpack("password_list")
-    if file_name.endswith('.rar'):
-        dic_unpack.rar_attack(file_name)
-    elif file_name.endswith('.zip'):
-        dic_unpack.zip_attack(file_name)
+    # dic_unpack = DicUnpack("password_list")
+    # if file_name.endswith('.rar'):
+    #     dic_unpack.rar_attack(file_name)
+    # elif file_name.endswith('.zip'):
+    #     dic_unpack.zip_attack(file_name)
 
 
 if __name__ == "__main__":
-    # file_name = sys.argv[1]
-    # main(file_name=file_name)
-    main(file_name="origin_file/test.rar")
+    args = arg_resolving()
+    main(args)
